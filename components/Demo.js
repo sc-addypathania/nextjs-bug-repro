0a74092bee9775a23bda79d344ab21f8977e5066
Addy Pathania <aditya.pathania@sitecore.com>
2022-06-28 10:31:26 -0400

    inital commit-for repro steps

---
+++ components/Demo.js
@@ -0,0 +1,18 @@
+﻿import { withComponentFactory } from "../context/GlobalState";
+import React from "react";
+
+class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // props not getting logged on the console.
+    // They get logged on client side.
+    console.log(this.props);
+
+    return <h2>Hello</h2>;
+  }
+}
+
+export const Demo1 = withComponentFactory(Demo);
---
+++ context/GlobalState.js
@@ -0,0 +1,21 @@
+﻿import { createContext } from "react";
+
+const StateContext = createContext();
+
+const StateProvider = ({ children, value }) => {
+  return (
+    <StateContext.Provider value={value}>{children}</StateContext.Provider>
+  );
+};
+
+export function withComponentFactory(Component) {
+  return function WithComponentFactory(props) {
+    return (
+      <StateContext.Consumer>
+        {(context) => <Component {...props} context={context} />}
+      </StateContext.Consumer>
+    );
+  };
+}
+
+export { StateProvider, StateContext };
